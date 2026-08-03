7870e803b11d606ce13dac2a69f2811680647aac
v86517 <katanayev@mail.ru>
2026-08-03 23:34:27 +1000

    interface 4

--- backend/templates/pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def privacy_policy(request):
-    return render(request, 'pages/privacy_policy.html')
-
-def consent(request):
-    return render(request, 'pages/consent.html')
